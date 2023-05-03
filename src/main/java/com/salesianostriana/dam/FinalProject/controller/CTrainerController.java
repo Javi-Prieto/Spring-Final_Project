7f45574c5cec199d28878e13525f5a754ab9feaa
Javier Prieto <jpojaviprior@gmail.com>
2023-05-03 14:29:40 +0200

    I've refresh the app for the database service

--- src/main/java/com/salesianostriana/dam/FinalProject/controller/CTrainerController.java
+++ src/main/java/com/salesianostriana/dam/FinalProject/controller/CTrainerController.java
@@ -1,5 +1,22 @@
 package com.salesianostriana.dam.FinalProject.controller;
 
-public class CTrainerController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.salesianostriana.dam.FinalProject.service.CTrainerService;
+
+@Controller
+public class CTrainerController {
+	@Autowired
+	private CTrainerService service;
+	
+	@GetMapping("/CtrainerList")
+	public String showCTrainer(Model model) {
+		model.addAttribute("CTrainerList", service.findAll());
+		
+		return "CTrainer";
+	}
+	
 }

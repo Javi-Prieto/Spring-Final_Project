110a1987101bf5fb7582c039145ae2fb9995cbbc
Javi Prieto <jpojaviprior@gmail.com>
2023-05-04 13:44:32 +0200

    *CONTROLADORES MENOS DELETE* En este caso lo que hemos hecho ha sido hacer los controladores necesarios para los formularios de añadir y de editar, faltando solo el de borrar además de comenzar con la adaptación de la página thymeleaf

--- src/main/java/com/salesianostriana/dam/FinalProject/controller/CTrainerController.java
+++ src/main/java/com/salesianostriana/dam/FinalProject/controller/CTrainerController.java
@@ -1,10 +1,15 @@
 package com.salesianostriana.dam.FinalProject.controller;
 
+
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.dam.FinalProject.model.ClassTrainer;
@@ -30,18 +35,37 @@ public class CTrainerController {
 	}
 	
 	
-	@GetMapping("/admin/CTrainerForm")
-	public String showCTrainerForm(Model model) {
+	@GetMapping("/admin/add/CTrainerForm")
+	public String showCTrainerAddForm(Model model) {
 		ClassTrainer cTrainer = new ClassTrainer();
-		model.addAttribute("CTrainerForm", cTrainer);
+		model.addAttribute("cTrainerForm", cTrainer);
 		return "TrainerForm";
 	}
 	
-	@PostMapping("/admin/CTrainerList")
-	public String submitForm(@ModelAttribute("CTrainerForm") ClassTrainer cTrainer, Model model) {
+	@PostMapping("/admin/add/CTrainerForm/submit")
+	public String submitAddForm(@ModelAttribute("cTrainerForm") ClassTrainer cTrainer, Model model) {
 		model.addAttribute("CTrainer", cTrainer);
 		
-		return "CTrainerAdmin";
+		return "redirect:/admin/CtrainerList";
+	}
+	
+	@GetMapping("/admin/edit/CTrainerForm/{codTrabajador}")
+	public String showCTrainerEditForm(@PathVariable("codTrabajador") Long codTrab, Model model) {
+		Optional<ClassTrainer> forEdit = service.findById(codTrab);
+		
+		if(forEdit.isPresent()) {
+			model.addAttribute("CTrainerForm", forEdit.get());
+			
+			return "TrainerForm";
+		}else {
+			return "redirect:/admin/CTrainerList";
+		}
+	
+	}
+	@PostMapping("/admin/edit/CTrainerForm/submit")
+	public String procesarFormularioEdicion(@ModelAttribute("cTrainerForm") ClassTrainer a) {
+		service.edit(a);
+		return "redirect:/admin/CTrainerList";
 	}
 	
 }

f0c9097c88e88367846c949ec01bb2db35e7401c
Javi Prieto <jpojaviprior@gmail.com>
2023-05-04 09:04:32 +0200

    *CONTROLADORES DE CLASS TRAINER* En este paso lo que he realizado es crear los controladores necesarios para gestionar las páginas de las vistas de entrenadores de sala y los formularios asociados a estos

--- src/main/java/com/salesianostriana/dam/FinalProject/controller/CTrainerController.java
+++ src/main/java/com/salesianostriana/dam/FinalProject/controller/CTrainerController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.salesianostriana.dam.FinalProject.model.ClassTrainer;
 import com.salesianostriana.dam.FinalProject.service.CTrainerService;
 
 @Controller
@@ -17,6 +20,28 @@ public class CTrainerController {
 		model.addAttribute("CTrainerList", service.findAll());
 		
 		return "CTrainer";
+	}
+	
+	@GetMapping("/admin/CtrainerList")
+	public String showAdminCTrainer(Model model) {
+		model.addAttribute("CTrainerList", service.findAll());
+		
+		return "CTrainerAdmin";
+	}
+	
+	
+	@GetMapping("/admin/CTrainerForm")
+	public String showCTrainerForm(Model model) {
+		ClassTrainer cTrainer = new ClassTrainer();
+		model.addAttribute("CTrainerForm", cTrainer);
+		return "TrainerForm";
+	}
+	
+	@PostMapping("/admin/CTrainerList")
+	public String submitForm(@ModelAttribute("CTrainerForm") ClassTrainer cTrainer, Model model) {
+		model.addAttribute("CTrainer", cTrainer);
+		
+		return "CTrainerAdmin";
 	}
 	
 }

3f010066a55024b2c82495647a26eca5dd878eb2
Javi Prieto <jpojaviprior@gmail.com>
2023-05-09 13:37:38 +0200

    *CAMBIOS VISUALES Y EN ATRIBUTOS* En este commite lo que he realizado es la adaptación visual de las páginas relacionadas con los personal trainer, además de añadir a los atributos de descripciones y url de imágenes la opción de que puedan contener más de 255 carácteres para que no de errores

---
+++ src/main/java/com/salesianostriana/dam/FinalProject/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.dam.FinalProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public String showIndex() {
+		return "portada";
+	}
+}
--- src/main/java/com/salesianostriana/dam/FinalProject/model/ClassTrainer.java
+++ src/main/java/com/salesianostriana/dam/FinalProject/model/ClassTrainer.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
@@ -24,7 +25,9 @@ public class ClassTrainer {
 	@GeneratedValue
 	private Long codTrabajador;
 	
-	private String nombre, email, desc, apellidos, dni, cuentaBanco, numTelefono, urlImg;
+	private String nombre, email, apellidos, dni, cuentaBanco, numTelefono;
+	@Lob
+	private String desc, urlImg;
 	private double salario;
 	private int experiencia;
 	

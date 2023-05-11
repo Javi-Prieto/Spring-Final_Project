38b2e825e288900ed2d49a1aa4b8f6e83dcb147e
Javi Prieto <jpojaviprior@gmail.com>
2023-05-11 10:11:30 +0200

    *SOLUCIÓN DE ERRORES Y GRANDES AVANCES* En este commit lo que hemos realizado ha sido la alteración del archivo sql para sacar la agregación de los dos usuarios mediante el sql. Para realizar esta inserción ahora vamos a usar una clase initdata para poder guardar la contraseña cifrada. Además de esto hemos realizado realizado una corrección de errores para solucionar y que funcione el login correctamente (tenía una letra de más en el label comparado con el input). He realizado también cambios en los fragmentos para añadir botones que solo quiero que vean los administradores, esto esta igual realizado en la página de la portada y cambio de la barra de navegación de las páginas Admin. Todavía falta:     Que los botones "Class Trainer admin" y "Entrenadores Admin" no aparezcan sino estas logeado como admin     Que desaparezca el boton de inscibirse y el de iniciar sesión una vez tenga una sesión iniciada     Posible borrado del fragmento navBarAdmin, ya que de funcionar lo primero que hemos mencionado en el todavía falta, no sería necesario este fragmento     Creación de un logout para poder quitar la sesión iniciada que solo aparezca una vez tenga una sesión iniciada

--- src/main/java/com/salesianostriana/dam/FinalProject/FinalProjectApplication.java
+++ src/main/java/com/salesianostriana/dam/FinalProject/FinalProjectApplication.java
@@ -2,10 +2,13 @@ package com.salesianostriana.dam.FinalProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.salesianostriana.dam.FinalProject.model.Client;
 
 @SpringBootApplication
 public class FinalProjectApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
 	}
---
+++ src/main/java/com/salesianostriana/dam/FinalProject/InitData.java
@@ -0,0 +1,61 @@
+package com.salesianostriana.dam.FinalProject;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.salesianostriana.dam.FinalProject.model.Client;
+import com.salesianostriana.dam.FinalProject.repositories.ClientRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class InitData {
+	
+	private final PasswordEncoder passwordEncoder;
+	private final ClientRepository servicio;
+	
+	@PostConstruct
+	public void init() {
+		
+		Client usuario = Client.builder()
+				.admin(false)
+				.username("user")
+				.password(passwordEncoder.encode("user"))
+				.bpReps(0)
+				.bpWeigh(0)
+				.sqReps(0)
+				.sqWeight(0)
+				.dlReps(0)
+				.dlWeight(0)
+				.cuentaBanco("0")
+				.numTelefono("0")
+				.cuotaMens(0)
+				.email("user@user.com")
+				.build();
+		
+		Client admin = Client.builder()
+				.admin(true)
+				.username("admin")
+				.password(passwordEncoder.encode("admin"))
+				.bpReps(0)
+				.bpWeigh(0)
+				.sqReps(0)
+				.sqWeight(0)
+				.dlReps(0)
+				.dlWeight(0)
+				.cuentaBanco("0")
+				.numTelefono("0")
+				.cuotaMens(0)
+				.email("admin@admin.com")
+				.build();
+		
+		servicio.saveAll(List.of(admin, usuario));		
+		
+	}
+
+}

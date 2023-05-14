b145fd62304f2209ed00700ea06117877e5c09a7
Javier Prieto <jpojaviprior@gmail.com>
2023-05-14 21:32:35 +0200

    *CLIENTE REGISTABLE* En este avance he conseguido que que los clientes se puedan inscribir al gimnasio y que este cliente sea funcional es decir que puedes acceder e iniciar sesión con el cliente que acabas de inscribir. Falta:     -La gestion de poder añadir cambios en los pesos y repeticiones y que estos se guarden.     -La gestión del cambio de altura o peso y que este se quede registrado.     -Que una vez registrado inmediatamente el cliente quede con la sesión iniciada.

--- src/main/java/com/salesianostriana/dam/FinalProject/controller/ClientController.java
+++ src/main/java/com/salesianostriana/dam/FinalProject/controller/ClientController.java
@@ -1,8 +1,37 @@
 package com.salesianostriana.dam.FinalProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.salesianostriana.dam.FinalProject.model.Client;
+import com.salesianostriana.dam.FinalProject.repositories.ClientRepository;
+import com.salesianostriana.dam.FinalProject.service.ClientService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ClientController {
-
+	@Autowired
+	private ClientService servicio;
+	private final PasswordEncoder passwordEncoder;
+	
+	@GetMapping("/register")
+	public String showRegisterForm(Model m) {
+		Client newCliente = new Client();
+		m.addAttribute("cliente", newCliente);
+		return "registerForm";
+	}
+	
+	@PostMapping("/register/submit")
+	public String submitRegisterForm(@ModelAttribute("cliente") Client cliente) {
+		cliente.setPassword(passwordEncoder.encode(cliente.getPassword()));
+		servicio.save(cliente);
+		return "redirect:/";
+	}
 }

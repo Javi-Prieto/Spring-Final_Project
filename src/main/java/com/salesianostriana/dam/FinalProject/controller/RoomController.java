819cc0a23ef7e8f71a751f861d64abc14a3c685d
Javier Prieto <jpojaviprior@gmail.com>
2023-05-23 23:41:14 +0200

    minimal changes

--- src/main/java/com/salesianostriana/dam/FinalProject/controller/RoomController.java
+++ src/main/java/com/salesianostriana/dam/FinalProject/controller/RoomController.java
@@ -1,10 +1,12 @@
 package com.salesianostriana.dam.FinalProject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.salesianostriana.dam.FinalProject.model.Client;
 import com.salesianostriana.dam.FinalProject.model.Reserve;
 import com.salesianostriana.dam.FinalProject.service.RoomService;
 
@@ -14,10 +16,11 @@ public class RoomController {
 	private RoomService service;
 	
 	@GetMapping("/room")
-	public String showRoomPage(Model m) {
+	public String showRoomPage(@AuthenticationPrincipal Client c ,Model m) {
 		Reserve res = new Reserve();
 		m.addAttribute("newReserve", res);
 		m.addAttribute("sala", service.findAll().get(0));
+		m.addAttribute("listC", service.findAll().get(0).getClients());
 		return "Sala";
 	}
 }
